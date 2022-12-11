d567bb80836d7dbab1b87e1e70d8acadceebb2ef
Abdurrahman Fatihoğlu <84565476+FTHGL@users.noreply.github.com>
2022-12-11 22:12:56 +0300

    LDR_Interface
    In this project, it is aimed to display the data read with the LDR sensor on the interface and save the data to the txt file.

---
+++ LDR_Interface/LDR_Interface/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LDR_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 282);
+            this.progressBar1.MarqueeAnimationSpeed = 50;
+            this.progressBar1.Maximum = 1023;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(121, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(417, 191);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "LDR Value";
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Gray;
+            this.start.Location = new System.Drawing.Point(156, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(79, 38);
+            this.start.TabIndex = 5;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Gray;
+            this.stop.Location = new System.Drawing.Point(255, 12);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(79, 38);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.Color.Gray;
+            this.reset.Location = new System.Drawing.Point(350, 12);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(79, 38);
+            this.reset.TabIndex = 7;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.Gray;
+            this.save.Location = new System.Drawing.Point(354, 282);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 8;
+            this.save.Text = "SAVE";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(442, 311);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button save;
+    }
+}
+
---
+++ LDR_Interface/LDR_Interface/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace LDR_Interface
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach(string port in ports)
+                comboBox1.Items.Add(port);
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e) 
+        {
+            data = serialPort1.ReadLine();  //veriyi al
+            this.Invoke(new EventHandler(displayData_event));
+        }
+        private void displayData_event(object sender, EventArgs e)
+        {
+            progressBar1.Value = Convert.ToInt16(data); //gelen değeri progress bar değerine eşitle
+            textBox1.Text += DateTime.Now.ToString() + "        " + data + "\n"; // gelen veriyi güncel zamanda textbar içinde gösterim
+            label2.Text = "LDR value =" + data; //veriyi label a ekle
+        
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text; //comboBox1 deki seçili ismi port a ata
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                stop.Enabled = true;
+                start.Enabled = false;
+
+            }
+            catch (Exception ex) { 
+            
+                MessageBox.Show(ex.Message,"hata");
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            stop.Enabled = false;
+            start.Enabled = true;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filelocation = @"**file path**";
+                string filename = "data.TXT";
+                System.IO.File.WriteAllText(filelocation + filename, "time\t\t\tValue\n" + textBox1.Text);
+                MessageBox.Show("dosya başarıyla kaydedildi", "Mesaj");
+            }
+            catch (Exception ex2) {
+
+                MessageBox.Show(ex2.Message, "hata");
+            }
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+        }
+    }
+}
